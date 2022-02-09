9ca7326a122f07814ed6e9e71634f08b481ccd74
tomas <72792707+tomas-land@users.noreply.github.com>
2022-02-09 23:04:55 +0200

    readme add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,7 @@
         integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    @stack('scripts')
+
 </body>
 
 </html>
